cb91951f947697a8c2ff3cf7de007c0d5d579a14
Anurag Bhardwaj <anuragbhardwaj.mait@gmail.com>
2017-04-11 15:52:23 +0530

    modified file appending

--- lib/generators/solidus_favorite_products/install/install_generator.rb
+++ lib/generators/solidus_favorite_products/install/install_generator.rb
@@ -5,7 +5,7 @@ module SolidusFavoriteProducts
       class_option :auto_run_migrations, type: :boolean, default: false
 
       def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/backend/spree_favorite_products\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_favorite_products\n"
         append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_favorite_products\n"
       end
 
