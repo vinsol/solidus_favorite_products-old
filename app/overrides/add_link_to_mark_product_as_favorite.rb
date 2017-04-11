8de72c5ba0a801cb23295c21b71d265a5763f371
Anurag Bhardwaj <anuragbhardwaj.mait@gmail.com>
2017-04-11 16:59:46 +0530

    added view/UI changes

--- app/overrides/add_link_to_mark_product_as_favorite.rb
+++ app/overrides/add_link_to_mark_product_as_favorite.rb
@@ -1,12 +1,15 @@
 Deface::Override.new(
   :virtual_path => 'spree/products/show',
   :name => 'add_link_to_mark_product_as_favorite',
-  :insert_after => "div[itemprop='description']",
+  :surround_contents => "h1[itemprop='name']",
   :text => %Q{
-    <% if spree_user_signed_in? && spree_current_user.has_favorite_product?(@product.id) %>
-      <%= link_to Spree.t(:unmark_as_favorite), favorite_product_path(:id => @product.id), :method => :delete, :remote => true, :class => 'favorite_link' %>
-    <% else %>
-      <%= link_to Spree.t(:mark_as_favorite), favorite_products_path(:id => @product.id), :method => :post, :remote => spree_user_signed_in?, :class => 'favorite_link' %>
+    <%= render_original %>
+    <% if spree_user_signed_in? %>
+      <% if spree_current_user.has_favorite_product?(@product.id) %>
+        <%= link_to Spree.t(:unmark_as_favorite), favorite_product_path(:id => @product.id), :method => :delete, :remote => true, :class => 'favorite_link button pull-right' %>
+      <% else %>
+        <%= link_to Spree.t(:mark_as_favorite), favorite_products_path(:id => @product.id), :method => :post, :remote => spree_user_signed_in?, :class => 'favorite_link button pull-right' %>
+      <% end %>
     <% end %>
   }
 )
