d8d6a40008e49f26134209357699858b1d56d119
Anurag Bhardwaj <anuragbhardwaj.mait@gmail.com>
2017-04-24 23:43:10 +0530

    added tabs for admin/product/favourite_uses, admin/users/favourite_products

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Spree::Core::Engine.routes.draw do
     resources :favorite_products, only: :index do
       get :users, on: :member
     end
+
+    resources :products do
+      get :favourite_users, on: :member
+    end
+
+    resources :users do
+      get :favourite_products, on: :member
+    end
   end
 
   resources :favorite_products, only: [:index, :create, :destroy]
