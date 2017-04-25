4723a40fa1774e13230423d9ec0d771b450efe56
Anurag Bhardwaj <anuragbhardwaj.mait@gmail.com>
2017-04-25 14:12:03 +0530

    added links on products_name and images, minor ui fixex

--- app/overrides/add_favorite_products_tab_ro_user_edit.rb
+++ app/overrides/add_favorite_products_tab_ro_user_edit.rb
@@ -3,7 +3,7 @@ Deface::Override.new(
   name: 'add_favorite_products_tab_user_edit',
   insert_bottom: '[data-hook="admin_user_tab_options"]',
   text: %Q{
-    <%= content_tag :li, class: ('active' if current == 'favorite_products') do %>
+    <%= content_tag :li, class: ('active' if current == :favorite_products) do %>
       <%= link_to Spree.t(:favorite_products,scope: [:admin, :tab]), spree.favorite_products_admin_user_path(@user) %>
     <% end if can?(:admin, Spree::Product) %>
   }
